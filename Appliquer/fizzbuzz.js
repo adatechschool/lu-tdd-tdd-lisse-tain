8a58da932e7436bff9161b54ca23cb5648c04d25
aliceKatkout <alice.katkout@gmail.com>
2021-03-16 10:42:01 +0100

    fizz

---
+++ Appliquer/fizzbuzz.js
@@ -0,0 +1,12 @@
+//boucle qui compte jusqua 100
+// on teste la divisibilité par 15, si oui print fizzbuzz
+// on teste la divisibilité par 3, si oui print fizz
+// on teste la divisibilité par 5, si oui print buzz
+
+function fizzbuzz(nombre) {
+  return nombre;
+}
+
+function main() {
+  fizzbuzz(0);
+}
